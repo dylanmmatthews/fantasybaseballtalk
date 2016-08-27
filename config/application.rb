89d3f3921f43bd70393a2419534b493460888360
Dylan Matthews <dylan.matthews1649@gmail.com>
2016-08-27 16:44:07 -0500

    ckeditor fix

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 
 module AllThingsFantasyBb
   class Application < Rails::Application
+
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
